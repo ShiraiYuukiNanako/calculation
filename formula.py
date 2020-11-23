84e1ebe9528ad128071a7d8f855951326ac055cf
LetaoXu <u5890621@anu.edu.au>
2020-11-23 15:31:14 +1100

    2

--- formula.py
+++ formula.py
@@ -1,13 +1,13 @@
-import math
+import mat
 
 def ph(h_plus):
-    return -math.log10(h_plus)
+    return -mat.log10(h_plus)
 
 def residence_time(Vriv,Criv,Vocean,Cocean):
     return (Vocean*Cocean)/(Vriv*Criv)
 
 def h_plus(ph):
-    return math.pow(10,-ph)
+    return mat.pow(10, -ph)
 
 def HCO3(ALK,DIC):
     return 2*DIC-ALK
@@ -16,7 +16,7 @@ def CO3(ALK,DIC):
     return ALK-DIC
 
 def pCO2(k0,k1,k2,ALK,DIC):
-    return (k2*math.pow(2*(DIC-ALK),2))/(k0*k1*(ALK-DIC))
+    return (k2 * mat.pow(2 * (DIC - ALK), 2)) / (k0 * k1 * (ALK - DIC))
 
 def saturation_state(Ca,CO3,ksp):
     return (Ca*CO3)/ksp
@@ -31,7 +31,7 @@ def burial(Vriv,Criv,Vmix,Cdeep,Csurf):
     return (Vriv*Criv)/(Vriv*Criv+Vmix*Cdeep-Vmix*Csurf)
 
 def light_tensity(z,a,I0):
-    return I0*math.exp(-a*z)
+    return I0 * mat.exp(-a * z)
 
 def Michaelis_Menten(Vmax,Cs,Km):
     return (Vmax*Cs)/(Km+Cs)
@@ -40,10 +40,10 @@ def Biological_pump(Cdeep,Csurf):
     return (Cdeep-Csurf)/Cdeep
 
 def stokes_sink(g,viscosity,radius,density_diff):
-    return 2*g*math.pow(radius,2)*(density_diff)/(9*viscosity)
+    return 2 * g * mat.pow(radius, 2) * (density_diff) / (9 * viscosity)
 
 def Martin(initial,z):
-    return initial*math.pow(z/100,-0.858)
+    return initial * mat.pow(z / 100, -0.858)
 
 def GAS_LAW(p,v,n,t,r):
     return p*v == n*t*r
@@ -52,8 +52,8 @@ def Henry(Kh,c):
     return Kh*c
 
 def AOU(T,S,O2):
-    return math.exp(177.7888 + 255.5907 /(T/100) + 146.4813* math.log(T/100,math.e)-22.204 * (T/ S*( 0.037362 +
-            (T/100)* (0.016504-0.0020564 * T/100)))* 1000/22.392)-O2
+    return mat.exp(177.7888 + 255.5907 / (T / 100) + 146.4813 * mat.log(T / 100, mat.e) - 22.204 * (T / S * (0.037362 +
+                                                                                                             (T/100) * (0.016504-0.0020564 * T/100))) * 1000 / 22.392) - O2
 
 def multiplicative_nutrient(Vmax,Fe,Si,Kmfe,Kmsi):
     return Vmax*(Fe/(Kmfe+Fe))*(Si/(Kmsi+Si))
@@ -65,24 +65,24 @@ def Activity(Lambda,n):
     return Lambda*n
 
 def radioactive_decay(N0,Lambda,t):
-    return N0*math.exp(-Lambda*t)
+    return N0 * mat.exp(-Lambda * t)
 
 def radioactive_growth(Np,Lambda,t):
-    return Np*(math.exp(Lambda*t)-1)
+    return Np*(mat.exp(Lambda * t) - 1)
 
 def mean_life(Lambda):
     return 1/Lambda
 
 def half_life(Lambda):
-    return math.log(2,math.e)/Lambda
+    return mat.log(2, mat.e) / Lambda
 
 def free_metal(Ctotal,logbeta_list,C_list,EDTA,Rinorganic):
     count = 1
     count += C_list[0]*logbeta_list[0]
-    count += math.pow(C_list[0],2)*logbeta_list[1]
-    count += C_list[1]*math.pow(10,logbeta_list[2])
-    count += C_list[2]*math.pow(10,logbeta_list[3])
+    count += mat.pow(C_list[0], 2) * logbeta_list[1]
+    count += C_list[1] * mat.pow(10, logbeta_list[2])
+    count += C_list[2] * mat.pow(10, logbeta_list[3])
     Y4 = EDTA/count
-    result = Ctotal/(math.pow(10,Rinorganic)+math.pow(10,logbeta_list[4])*Y4)
+    result = Ctotal/(mat.pow(10, Rinorganic) + mat.pow(10, logbeta_list[4]) * Y4)
     ratio = result/Ctotal*100
     return result,ratio
--- geoscience.py
+++ geoscience.py
@@ -1,10 +1,62 @@
-import math
+import csv
+import mat
+
+hole = list()
+NS, EW, GRADE = dict(), dict(), dict()
+
+with open('kriging.csv', newline='') as csvfile:
+    file = csv.reader(csvfile)
+    for row in file:
+        hole.append(row[0])
+        EW[row[0]] = row[1]
+        NS[row[0]] = row[2]
+        GRADE[row[0]] = row[3]
+
+NS.pop('HOLE')
+EW.pop('HOLE')
+GRADE.pop('HOLE')
+hole.remove('HOLE')
 
 
-def circle(radius):
-    diameter = 2*radius
-    c = diameter*math.pi
-    return c
+def NS_count_pair(lag_distance):
+    result = []
+    for i in range(len(hole)):
+        for j in range(len(hole)):
+            if int(NS[hole[i]]) - int(NS[hole[j]]) == lag_distance and int(EW[hole[i]]) == int(EW[hole[j]]):
+                result.append((hole[i],hole[j]))
+    return result
 
 
-print(circle(3))
+def EW_count_pair(lag_distance):
+    result = []
+    for i in range(len(hole)):
+        for j in range(len(hole)):
+            if int(EW[hole[i]]) - int(EW[hole[j]]) == lag_distance and int(NS[hole[i]]) == int(NS[hole[j]]) :
+                result.append((hole[i],hole[j]))
+    return result
+
+
+lag_distance = [200, 400, 600, 800, 1000]
+
+NS_count, EW_count = dict(), dict()
+
+for lag in lag_distance:
+    NS_count[lag] = len(NS_count_pair(lag))
+    EW_count[lag] = len(EW_count_pair(lag))
+
+print(NS_count)
+print(EW_count)
+
+
+def gamma(h):
+    grade_sum = 0
+    for pair in EW_count_pair(h):
+        grade_sum += mat.pow(float(GRADE[pair[0]]) - float(GRADE[pair[1]]), 2)
+    return grade_sum/(2*len(NS_count_pair(h)))
+
+
+gammas = dict()
+
+for lag in lag_distance:
+    gammas[lag] = gamma(lag)
+print(gammas)
